827ae879d2d5ad447fff639c2168fa491747e794
Akbar <Akbar@DESKTOP-16DASBH>
2024-07-15 23:08:45 +0330

    Add Controllers

---
+++ API/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using API.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private IPaymentService _TransactionService;
+        private readonly IHttpContextAccessor _ContextAccessor;
+        public PaymentController(IHttpContextAccessor contextAccessor, IPaymentService transactionService)
+        {
+            _ContextAccessor = contextAccessor;
+            _TransactionService = transactionService;
+        }
+
+        [HttpGet("Verify")]
+        public async Task<IActionResult> Verify(long trackId)
+        {
+            return Ok(await _TransactionService.Verify(trackId));
+        }
+    }
+}
